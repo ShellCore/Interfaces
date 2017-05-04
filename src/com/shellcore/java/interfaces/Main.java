4c0d7851bfc8c484bf45bb66b2eecf0ee0fdc5d0
Cesar Morales Garduño <shell.one.core@gmail.com>
2017-05-04 12:19:06 -0500

    Creación de la clase Motocicleta, que también implementa la interfaz Vehículo.

--- src/com/shellcore/java/interfaces/Main.java
+++ src/com/shellcore/java/interfaces/Main.java
@@ -8,5 +8,8 @@ public class Main {
     public static void main(String[] args) {
         Car taxi = new Car(4);
         System.out.println("¿El taxi tiene ruedas? " + taxi.hasWheels() + "(" + taxi.getNumberOfWheels() + ")");
+
+        Motocycle motoTaxi = new Motocycle(2);
+        System.out.println("¿El moto-taxi tiene ruedas? " + motoTaxi.hasWheels() + "(" + motoTaxi.getNumberOfWheels() + ")");
     }
 }
